d1185dfbecce91356fb1c907ee2456900dd31abc
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-12-07 11:02:49 +0200

    moved to raylib

--- src/main/java/game/Codecraft.java
+++ src/main/java/game/Codecraft.java
@@ -1,51 +1,20 @@
 package game;
 
-import game.engine.Time;
-import game.engine.Window;
-
-import java.io.IOException;
-
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
-import static org.lwjgl.opengl.GL11.*;
+import static com.raylib.Jaylib.*;
+import static com.raylib.Raylib.*;
 
 public class Codecraft implements Runnable {
-    private Window window;
 
     @Override
     public void run() {
-        window = Window.get();
+        InitWindow(1280, 720, "Codecraft");
 
-        Game game;
-        try {
-            game = new Game();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        while (!WindowShouldClose()) {
+            BeginDrawing();
+            ClearBackground(RED);
+            DrawRectangle(100, 200, 100, 250, BLUE);
+            DrawCircle(400, 400, 50, YELLOW);
+            EndDrawing();
         }
-
-        float beginTime = Time.getTime();
-        float endTime;
-
-        while (window.isOpened()) {
-            glfwPollEvents();
-
-            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            if (Time.deltaTime >= 0) {
-                game.update();
-            }
-
-            window.swapBuffers();
-
-            endTime = Time.getTime();
-            Time.deltaTime = endTime - beginTime;
-            beginTime = endTime;
-        }
-
-        window.free();
-
-        glfwTerminate();
-        glfwSetErrorCallback(null).free();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,8 @@
 module com.example.codecraft {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.lwjgl;
-    requires org.lwjgl.glfw;
-    requires org.lwjgl.opengl;
+    requires org.joml;
+    requires jaylib;
 
     exports com.example.codecraft.idea;
     opens com.example.codecraft.idea to javafx.fxml;
