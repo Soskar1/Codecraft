e35f8b22468ece4d11138342eadd9fbea561d839
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-12-09 17:09:07 +0200

    loading file content

--- src/main/java/com/example/codecraft/idea/IDEAController.java
+++ src/main/java/com/example/codecraft/idea/IDEAController.java
@@ -3,14 +3,26 @@ package com.example.codecraft.idea;
 import com.example.codecraft.Settings;
 import com.example.codecraft.game.Codecraft;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 
 import java.io.*;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ResourceBundle;
 
-public class IDEAController {
+public class IDEAController implements Initializable {
     @FXML private TextArea codeTextArea;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            loadContent();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public void runGame() throws IOException {
         saveContent();
@@ -19,17 +31,28 @@ public class IDEAController {
         gameThread.start();
     }
 
-    public void saveContent() throws IOException {
+    private void loadContent() throws IOException {
         Files.createDirectories(Paths.get(Settings.pythonSourceDirectory));
-
         File pythonScript = new File(Settings.fullPythonSourcePath);
 
-        if (pythonScript.isFile() || pythonScript.createNewFile()) {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(pythonScript));
-            writer.write(codeTextArea.getText());
-            writer.close();
+        if (pythonScript.isFile()) {
+            BufferedReader reader = new BufferedReader(new FileReader(pythonScript));
+
+            String line;
+            String newLine = System.getProperty("line.separator");
+            while ((line = reader.readLine()) != null)
+                codeTextArea.appendText(line + newLine);
+
+            reader.close();
         } else {
-            throw new IOException("Can't write to a file!");
+            pythonScript.createNewFile();
         }
+    }
+
+    private void saveContent() throws IOException {
+        File pythonScript = new File(Settings.fullPythonSourcePath);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(pythonScript));
+        writer.write(codeTextArea.getText());
+        writer.close();
     }
 }
